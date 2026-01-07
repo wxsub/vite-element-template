d336dfa123560f1d27b10a2a2dbfa4cdaeeeca3b
NicolasHome <1556363381@qq.com>
2026-01-07 15:14:25 +0800

    Add dynamic parameter route page
    Created _id.vue under nested/dynamic-parameters to display dynamic route parameter values. Includes both Chinese and English descriptions for the parameter.

---
+++ src/pages/nested/dynamic-parameters/_id.vue
@@ -0,0 +1,12 @@
+<route>
+{
+  meta: { title: "动态参数路由" }
+}
+</route>
+
+<template>
+  <div>
+    <h1>这是动态参数路由, 参数值为: {{ $route.params.id }}</h1>
+    <p>This is dynamic parameter routing. parameter value: {{ $route.params.id }}</p>
+  </div>
+</template>
