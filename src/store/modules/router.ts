a24db4ad0f998f3765e4b76e6dcb7c7da83a425c
NicolasHome <1556363381@qq.com>
2026-02-04 02:35:15 +0000

    feat(router): Added SidebarMenus to support dynamic management of sidebar menus

--- src/store/modules/router.ts
+++ src/store/modules/router.ts
@@ -1,13 +1,18 @@
 import { defineStore } from "pinia"
 import { store } from "@/store"
 
+const ConstantMenus = [
+  { name: "首页", path: "/", icon: "House" }
+];
+
 export const routerStore = defineStore("router", () => {
   const whiteList: Array<string> = [],
+    SidebarMenus: any = ref([]),
     routers: any = ref([]);
 
   const setRoutes = (data: Array<object> | any) => routers.value = data
 
-  return { routers, setRoutes, whiteList }
+  return { routers, setRoutes, whiteList, SidebarMenus }
 })
 
 export function useRouterStoreHook() {
